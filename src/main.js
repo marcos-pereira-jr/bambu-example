e6460ed2636aef02eafc36efb42d327c80a4886c
marcos-pereira-jr <marcos.ant.pereira.jr@gmail.com>
2020-10-24 21:12:22 -0300

    Componene usuario

--- src/main.js
+++ src/main.js
@@ -3,7 +3,8 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-
+import '@fortawesome/fontawesome-free/css/all.css';
+import '@fortawesome/fontawesome-free/js/all';
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
