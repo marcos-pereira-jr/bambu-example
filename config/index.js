7769a6fb98d79fb352828b21384e194ccd09e2b1
marcos-pereira-jr <marcos.ant.pereira.jr@gmail.com>
2020-10-25 10:25:46 -0300

    Deploy

--- config/index.js
+++ config/index.js
@@ -49,8 +49,8 @@ module.exports = {
 
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
-    assetsSubDirectory: '/static',
-    assetsPublicPath: '/bambu',
+    assetsSubDirectory: 'static',
+    assetsPublicPath: '/bambu/',
 
     /**
      * Source Maps
